e41cbd27b8223c9e9576ba71db27c8a018f58b9c
Cara Lin <caralin@usc-guestwireless-upc-newsc18564.usc.edu>
2020-02-01 23:02:45 -0800

    separate

--- flask intro/app.py
+++ flask intro/app.py
@@ -3,7 +3,12 @@ from dataFrameTest import simple_page
 
 app = Flask(__name__)
 app.register_blueprint(simple_page)
-help = []
+#help = []
+feels = []
+emotions = []
+sleeps = []
+assignments = []
+googles = []
 
 @app.route('/', methods=["GET"])
 def index():
@@ -28,15 +33,15 @@ def HackSCHomePage():
 @app.route('/addfeel', methods = ['POST', 'GET'])
 def addfeel():
     feel = request.form["feel"]
-    help.append(feel)
-    print(help)
+    feels.append(feel)
+    print(feels)
     return render_template('inputs.html', feel=feel)
 
 @app.route('/addemotion', methods = ['POST', 'GET'])
 def addemotion():
     emotion = request.form["emotion"]
-    help.append(emotion)
-    print(help)
+    emotions.append(emotion)
+    print(emotions)
     return render_template('inputs.html', emotion=emotion)
 
 @app.route('/addsleep', methods = ['POST', 'GET'])
@@ -46,36 +51,30 @@ def addsleep():
     sleep.append(weekday)
     weekend = request.form["weekend"]
     sleep.append(weekend)
-    help.append(sleep)
-    print(help)
+    sleeps.append(sleep)
+    print(sleeps)
     return render_template('inputs.html', weekday=weekday, weekend=weekend)
 
 @app.route('/addassign', methods = ['POST', 'GET'])
 def addassign():
     assigns = []
-    a1 = request.form["assignment1"]
-    assigns.append(a1)
-    a2 = request.form["assignment2"]
-    assigns.append(a2)
-    a3 = request.form["assignment3"]
-    assigns.append(a3)
-    a4 = request.form["assignment4"]
-    assigns.append(a4)
-    a5 = request.form["assignment5"]
-    assigns.append(a5)
-    a6 = request.form["assignment6"]
-    assigns.append(a6)
-    a7 = request.form["assignment7"]
-    assigns.append(a7)
-    help.append(assigns)
-    print(help)
+    name = request.form["aname"]
+    assigns.append(name)
+    priority = request.form["apriority"]
+    assigns.append(priority)
+    workload = request.form["aworkload"]
+    assigns.append(workload)
+    date = request.form["adate"]
+    assigns.append(date)
+    assignments.append(assigns)
+    print(assignments)
     return render_template('inputs.html', a1=a1, a2=a2, a3=a3, a4=a4, a5=a5, a6=a6, a7=a7)
 
 @app.route('/addgoogle', methods = ['POST', 'GET'])
 def addgoogle():
     google = request.form["google"]
-    help.append(google)
-    print(help)
+    googles.append(google)
+    print(googles)
     return render_template('inputs.html', google=google)
 
 
